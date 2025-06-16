046399a253233563c782535a0d3dce71b3e1252a
Ary Ferreira <aryjunior@gmail.com>
2025-06-16 16:38:41 -0300

    Adjusting the DataOps service test to my needs

--- src/test/java/org/doi/prmv4p113603/mlops/service/DataOpsServiceTest.java
+++ src/test/java/org/doi/prmv4p113603/mlops/service/DataOpsServiceTest.java
@@ -1,5 +1,8 @@
 package org.doi.prmv4p113603.mlops.service;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.doi.prmv4p113603.mlops.config.MlopsProperties;
 import org.doi.prmv4p113603.mlops.data.dto.NominalCompositionDto;
 import org.doi.prmv4p113603.mlops.data.request.ScheduleExplorationRequest;
@@ -7,14 +10,12 @@ import org.doi.prmv4p113603.mlops.domain.*;
 import org.doi.prmv4p113603.mlops.model.*;
 import org.doi.prmv4p113603.mlops.repository.NominalCompositionRepository;
 import org.doi.prmv4p113603.mlops.repository.RunRepository;
-import org.doi.prmv4p113603.mlops.util.SimulationArtifactFactory;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.*;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.*;
-import java.util.stream.IntStream;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
@@ -84,33 +85,6 @@ class DataOpsServiceTest {
         /*
          * Mockito configuration:
          *
-         * Mock 'SimulationDirectories' (manual wiring).
-         *
-         * Create a fake instance of 'SimulationDirectory' that it can be controlled in
-         * this unit test; but that doesn’t do any real work.
-         */
-        SimulationDirectory subDir = mock(SimulationDirectory.class);
-        SimulationDirectory runDir = mock(SimulationDirectory.class);
-        when(runDir.getNumber()).thenReturn(1);
-        when(runDir.getChildren()).thenReturn(List.of(subDir));
-
-        SimulationDirectory nominalDir = mock(SimulationDirectory.class);
-        when(nominalDir.getChildren()).thenReturn(List.of(runDir));
-
-        SimulationDirectories dirs = mock(SimulationDirectories.class);
-        when(dirs.getNominalCompositionDir()).thenReturn(nominalDir);
-
-        /*
-         * Mockito configuration:
-         *
-         * When the method 'load(true)' is called on the mock 'dirs', do nothing
-         * (i.e., suppress any real behavior and return void silently).
-         */
-        doNothing().when(dirs).load(true);
-
-        /*
-         * Mockito configuration:
-         *
          * Spy the service to inject mock 'SimulationDirectories'. It will create a partial
          * mock of the real 'DataOpsService' object and its real methods will run unless they
          * are explicitly stub or override.
@@ -118,59 +92,28 @@ class DataOpsServiceTest {
         DataOpsService service = spy(new DataOpsService(compositionRepo, runRepo, mlopsProperties));
 
         /*
-         * Mockito configuration:
+         * Acting to get the response payload in the same way the controller does.
          *
-         * A spy is a Mockito object that wraps a real instance but allows overriding just
-         * specific methods and let the rest of the object behave normally.
-         *
-         * This is what we have when the protected method createSimulationDirectories is called.
-         *
-         * Why 'doReturn(...).when(...)' instead of 'when(...).thenReturn(...)'?
-         * Because service is a spy, and Mockito’s 'when(...).thenReturn(...)' eagerly calls
-         * the real method before it can stub it.
+         * NOTE: that process will scan directories, what will turn this unit test into
+         *  an integration test, since we will be testing how the service integrates
+         *  with the real filesystem. There are some trade-offs since there is an
+         *  external dependence introduced but that data discovery portion is needed here.
          */
-        IntStream.rangeClosed(1, numSimulations).forEach( runNumber ->
-                doReturn(dirs).when(service).createSimulationDirectories(nominalCompositionName, dataRoot, runNumber, numSimulations)
-        );
-
-        /*
-         * Mockito configuration:
-         *
-         * Mock the factory static call (optionally replace with injectable strategy).
-         *
-         * Configures Mockito to intercept all static method calls to 'SimulationArtifactFactory'
-         * within this test scope, so it is possible to control their return values.
-         */
-        List<SimulationArtifact> fakeArtifacts = List.of(mock(SimulationArtifact.class));
-
-        /*
-         * Mockito configuration:
-         *
-         * By default, Mockito cannot mock static methods — they are hardwired at the class level.
-         *
-         * Telling Mockito that the static method calls to 'SimulationArtifactFactory' will  be
-         * intercepted and Mockito returns a 'MockedStatic' object that can be used to define behavior.
-         */
-        MockedStatic<SimulationArtifactFactory> factory = mockStatic(SimulationArtifactFactory.class);
-
-        /*
-         * Here the call to the static method 'load' is being intercepted and its behavior is being
-         * mocked to return 'fakeArtifacts', instead of doing real file discovery/parsing.
-         */
-        factory.when(() -> SimulationArtifactFactory.load(eq(nominalCompositionName), any(), eq(subDir)))
-                .thenReturn(fakeArtifacts);
-
-        // Acting to get the response payload in the same way the controller does
         NominalCompositionDto result = service.scheduleExploration(nominalCompositionName, request);
 
-        System.out.println("ARYLOG: result.toString() -> " + result.toString());
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.enable(SerializationFeature.INDENT_OUTPUT);
+        mapper.registerModule(new JavaTimeModule());
+        mapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+
+        String resultJson = mapper.writeValueAsString(result);
+
+        System.out.println("ARYLOG: resultJson -> " + resultJson);
 
         // Asserting with Junit 5
         assertNotNull(result);
         assertEquals(numSimulations, result.getRuns().size());
         assertEquals(SimulationStatus.SCHEDULED, result.getRuns().get(0).getStatus());
-
-        factory.close(); // Closing the static mock to avoid test pollution.
 
     }
 
