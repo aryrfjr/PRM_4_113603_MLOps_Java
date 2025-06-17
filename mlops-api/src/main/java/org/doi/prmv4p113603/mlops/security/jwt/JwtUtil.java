e74b7bd15090d1ba5f714c91aa5157f829df6153
Ary Ferreira <aryjunior@gmail.com>
2025-06-16 23:50:02 -0300

    Adjustments in JWT secret key.

--- mlops-api/src/main/java/org/doi/prmv4p113603/mlops/security/jwt/JwtUtil.java
+++ mlops-api/src/main/java/org/doi/prmv4p113603/mlops/security/jwt/JwtUtil.java
@@ -4,24 +4,40 @@ import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
 
+import java.nio.charset.StandardCharsets;
+import java.security.Key;
 import java.util.Date;
 
 @Component
 public class JwtUtil {
-    private final String secret = "your-secret-key";
-    private final long expiration = 1000 * 60 * 60; // 1 hour
+
+    /*
+     * TODO: A stronger key could be generated with:
+     *
+     *  private static final Key key = Keys.secretKeyFor(SignatureAlgorithm.HS256);
+     *
+     *  In this case, it will be necessary to persist or export the key, or a new one
+     *  will be generated every time the app starts.
+     */
+    @Value("${jwt.secret}")
+    private String secret;
 
     public String generateToken(UserDetails userDetails) {
+
+        long expiration = 1000 * 60 * 60; // 1 hour
+
         return Jwts.builder()
                 .setSubject(userDetails.getUsername())
                 .claim("roles", userDetails.getAuthorities())
                 .setIssuedAt(new Date())
                 .setExpiration(new Date(System.currentTimeMillis() + expiration))
-                .signWith(Keys.hmacShaKeyFor(secret.getBytes()), SignatureAlgorithm.HS256)
+                .signWith(getSigningKey(), SignatureAlgorithm.HS256)
                 .compact();
+
     }
 
     public String extractUsername(String token) {
@@ -29,10 +45,17 @@ public class JwtUtil {
     }
 
     public Claims getClaims(String token) {
+
         return Jwts.parserBuilder()
-                .setSigningKey(secret.getBytes())
+                .setSigningKey(getSigningKey())
                 .build()
                 .parseClaimsJws(token)
                 .getBody();
+
     }
+
+    private Key getSigningKey() {
+        return Keys.hmacShaKeyFor(secret.getBytes(StandardCharsets.UTF_8));
+    }
+
 }
